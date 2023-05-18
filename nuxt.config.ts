df4006df49be4eba3c4f7ce66517363ed6763803
iman <iman.ghavasieh@gmail.com>
2023-05-19 00:29:11 +0330

    feat: set head title for the app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'Vue Blog',
+        },
+    },
     modules: ['@pinia/nuxt'],
     css: ['~/assets/css/main.css'],
     postcss: {

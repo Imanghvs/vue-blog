5f63648f08aa3841472df16da63b46db0eba63f8
iman <iman.ghavasieh@gmail.com>
2023-05-19 00:48:46 +0330

    fix: comment input restrictions

--- components/CommentInput.vue
+++ components/CommentInput.vue
@@ -18,9 +18,12 @@ export default {
         ...mapActions(useMainStore, ['submitComment']),
         async handleSubmit(event: any) {
             event.preventDefault();
+            if (this.body.trim().length < 2) {
+                return;
+            }
             await this.submitComment({
                 commentId: this.comments[0].commentId + 1,
-                body: this.body,
+                body: this.body.trim(),
                 postId: Array.isArray(this.postId) ? this.postId[0] : this.postId,
                 name: 'You',
                 date: new Date().toString(),
@@ -38,14 +41,16 @@ export default {
                 <h2 class="px-4 pt-3 pb-2 text-gray-800 text-lg">Add a new comment</h2>
                 <div class="w-full md:w-full px-3 mb-2 mt-2">
                     <textarea
-                        class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"
-                        name="body" v-model="body" placeholder='Type Your Comment' required></textarea>
+                        class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-400 focus:outline-none focus:bg-white"
+                        name="body" v-model="body" placeholder='Type Your Comment' required maxlength="512">
+                                </textarea>
                 </div>
                 <div class="w-full md:w-full flex items-start md:w-full px-3">
                     <div class="-mr-1">
-                        <input type='submit'
-                            class="bg-white text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100"
-                            value='Post Comment'>
+                        <input type='submit' disabled :class="[
+                            body.trim().length > 2 ? 'cursor-pointer bg-white hover:bg-gray-100 text-gray-700' : 'bg-gray-200 text-gray-400',
+                            'font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1'
+                        ]" value='Post Comment' />
                     </div>
                 </div>
             </div>
